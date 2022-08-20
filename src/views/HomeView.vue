0a8853731c80e1082a33109e735fa46f34b3aec8
ricardovanlaarhoven <ricardo1994@gmail.com>
2022-08-20 12:40:41 +0200

    chore: added a wip grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+enum Color {
+  green,
+}
+const gameGrid: {color: Color, star: boolean}[][] = [
+    [
+      {color: Color.green, star: false},
+      {color: Color.green, star: false},
+    ]
+]
 </script>
 
 <template>
   <main>
-    Hi all :)
+    <div v-for="(row, rowIndex) in gameGrid" :key="rowIndex">
+      <div>
+        <div v-for="(column, columnIndex) in row" :key="columnIndex">
+          {{column.color}}
+          {{column.star}}
+        </div>
+      </div>
+    </div>
   </main>
 </template>
